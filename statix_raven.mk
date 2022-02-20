c9439ba1a4ce4f1b77ee8feb3c71400d2667ff8b
Anay Wadhera <anay1018@gmail.com>
2022-02-20 18:35:46 +0900

    raven: update to SQ1D.220205.004
    Change-Id: I6aa766473d398913c2b72ea12cbe73860c1dc7ad
    Reviewed-on: https://review.statixos.com/c/android_device_google_raviole/+/6921
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>

--- statix_raven.mk
+++ statix_raven.mk
@@ -20,8 +20,8 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=raven \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.004 8151327 release-keys"
 
-BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.220205.004/8151327:user/release-keys
 
 $(call inherit-product, vendor/google/raven/raven-vendor.mk)
